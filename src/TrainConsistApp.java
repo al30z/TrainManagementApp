63508fe456070b6158bb5dcf009ae778f95bfa2b
al30z <arushilakhera30@gmail.com>
2026-04-17 16:20:25 +0530

    Basic app setup

---
+++ src/TrainConsistApp.java
@@ -0,0 +1,8 @@
+public class TrainConsistApp {
+    public static void main(String[] args) {
+        System.out.println("Welcome to Train Consist Management System!");
+
+        int initialBogies = 0;
+        System.out.println("Initial bogie count: " + initialBogies);
+    }
+}
